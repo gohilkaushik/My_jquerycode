984b8635a8822dc2d088976f10c4ddfe0b1bc91d
Gohil Kaushik <93031942+gohilkaushik@users.noreply.github.com>
2022-08-03 12:31:29 +0530

    Add files via upload

---
+++ My_jquerycode.js
@@ -0,0 +1,16 @@
+// Accordin js
+$('.accordin-title').click(function(e){
+    e.preventDefault();
+    const accordinTitle = $(this).closest('.accordin-title');
+    accordinTitle.next().toggleClass('show');
+
+    const icon = accordinTitle.find("i");
+    if(icon.hasClass("fa-plus-circle")){
+        icon.removeClass('fa-plus-circle');
+        icon.addClass('fa-minus-circle')
+    }
+    else if(icon.hasClass("fa-minus-circle")){
+        icon.addClass('fa-plus-circle');
+        icon.removeClass('fa-minus-circle')
+    };
+})
